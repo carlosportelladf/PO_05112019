bf52de4609716b2f0e16834271467bc6489188b6
adrianadrica <adrianadrica0408@gmail.com>
2019-11-05 20:51:59 -0300

    Primeiro commit do main.py

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # PO_05112019
-
 acrescentando uma linha
 enviando
---
+++ main.py
@@ -0,0 +1,9 @@
+a = 1
+b = -1
+soma = a + b
+if (soma > a):
+    print ("a soma eh maior que a")
+elif(soma == a):
+    print ("a soma eh igual a a")
+else:
+    print ("a soma eh menor que a")
