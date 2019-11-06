7561087f64729c6d367594be8327151fe56719b3
adrianadrica <adrianadrica0408@gmail.com>
2019-11-05 21:36:43 -0300

    main com string

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+pessoaNome = "Portella"
 a = 1
 b = -1
 
@@ -5,8 +6,14 @@ b = -1
 soma = a + b
 # testes para mostrar o resultado
 if (soma > a):  #caso a soma seja maior do que a variavel a
-    print ("a soma eh maior que a")
+    print (pessoaNome + ", a soma eh maior que a")
 elif(soma == a): #caso a soma seja igual do que a variavel a
-    print ("a soma eh igual a a")
+    print (pessoaNome +  ", a soma eh igual a a")
 else: #caso a soma seja menor do que a variavel a
-    print ("a soma eh menor que a")
+    print (pessoaNome +  ", a soma eh menor que a")
+# trabalhando com strings    
+print ("a primeira letra do nome eh ", pessoaNome[0])
+print ("a segunda letra do nome eh ", pessoaNome[1])
+#Funcao len indica o tamanho da string
+# subtrai um pois começa do zero
+print ("a ultima letra do nome eh ", pessoaNome[len(pessoaNome) - 1 ])
