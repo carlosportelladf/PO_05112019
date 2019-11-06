ff0bd758cc74cb827575536b7bacc84d878d720f
adrianadrica <adrianadrica0408@gmail.com>
2019-11-05 21:06:49 -0300

    Agora com tudo comentado

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 a = 1
 b = -1
+
+# efetua a soma das variaveis
 soma = a + b
-if (soma > a):
+# testes para mostrar o resultado
+if (soma > a):  #caso a soma seja maior do que a variavel a
     print ("a soma eh maior que a")
-elif(soma == a):
+elif(soma == a): #caso a soma seja igual do que a variavel a
     print ("a soma eh igual a a")
-else:
+else: #caso a soma seja menor do que a variavel a
     print ("a soma eh menor que a")
